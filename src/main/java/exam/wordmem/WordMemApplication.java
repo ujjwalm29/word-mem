a16cb8c3b4df76120d6d85b05d1aa8cbc90c526f
Ujjwal Maheshwari <ujjwal@Ujjwals-MacBook-Pro.local>
2023-02-27 00:02:45 -0500

    initial drop

---
+++ src/main/java/exam/wordmem/WordMemApplication.java
@@ -0,0 +1,13 @@
+package exam.wordmem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordMemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordMemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/exam/wordmem/WordMemApplicationTests.java
@@ -0,0 +1,13 @@
+package exam.wordmem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordMemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
